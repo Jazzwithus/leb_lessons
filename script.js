00fe1ad24607bacd0e142712779ad4450bc82575
Yukoridy <yukonridy@gmail.com>
2022-10-24 23:09:06 +0300

    Первое сохранение

---
+++ script.js
@@ -0,0 +1,33 @@
+// const carBodyWidth = 5000;
+// const carBodyLeght = 4000;
+
+//console.log ("Ширина кузова автомобиля " + carBodyWidth + ' Длина автомобиля ' + carBodyLeght);\
+
+// let answer =+ prompt ("Введите ваш возраст", "18");
+// console.log (answer);
+
+// const answers = [];
+
+// answers [0] = prompt ("Как Вас зовут?" , "Ваня");
+// answers [1] = prompt ("Сколько вам лет?" , "16");
+// answers [2] = prompt ("Ваш город жительства" , "Москва");
+
+// console.log (answers);
+// document.write(typeOf(answers));
+
+// "use strict";
+
+// const category = 'toys'; 
+// console.log (`htpss://yandex.ru/${category}/5`);
+
+// let user ='Ivan';
+// alert (`Привет ${ user}`);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log (incr);
+    console.log (decr);
