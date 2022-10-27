c6117da763f7e4e3d9b4ea5f393eb555f1559c78
Yukoridy <yukonridy@gmail.com>
2022-10-28 00:49:25 +0300

    Записаны объекты и промты

---
+++ script.js
@@ -0,0 +1,17 @@
+var numberOfFilms = promt ("Сколько фильмов вы посмотрели", '');
+
+var personalMovieDB = {
+    count : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+    
+const a = prompt ('Один из последних просмотренных фильмов', ''),
+      b = prompt  ('На сколько оцените его', ''),
+      c = prompt ('Один из последних просмотренных фильмов', ''),
+      d = prompt  ('На сколько оцените его', '');
+
+
+
