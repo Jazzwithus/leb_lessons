6e20d67cb5f68a1bded9c988bda394aaa398eeb0
Yukoridy <yukonridy@gmail.com>
2022-10-28 22:23:31 +0300

    Пройдет урок про кейсы

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var numberOfFilms = prompt ("Сколько фильмов вы посмотрели", '');
 
-var personalMovieDB = {
-    count : numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-    
-const a = prompt ('Один из последних просмотренных фильмов', ''),
-      b = prompt  ('На сколько оцените его', ''),
-      c = prompt ('Один из последних просмотренных фильмов', ''),
-      d = prompt  ('На сколько оцените его', '');
+if (num < 49) {
+    console.log ('Цифра меньше заданной');
+} else if (num > 100) {
+    console.log ('Слишком много');
+} else {
+    console.log ('Все в порядке');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+(num === 50) ? console.log ('Все в порядке') : console.log ('Цифра меньше заданной');
 
-console.log (personalMovieDB);
+const num = 150;
 
+switch (num) {
+    case 40:
+        console.log ('Меньше , чем нужно');
+        break;
+    case 45:
+        console.log ('Точно в цель, Яблочко!!!');
+        break;
+    case 50:
+        console.log ('Уже ближе к пятидесяти');
+        break;
+    default:
+        console.log ('По дефолту');
+        break;
+}
